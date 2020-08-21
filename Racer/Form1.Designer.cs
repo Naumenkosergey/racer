daba4ef30cdad028da01965171854d0c99ff61b9
Naumenkosergey <43389318+Naumenkosergey@users.noreply.github.com>
2020-08-22 00:21:52 +0300

    Добавьте файлы проекта.

---
+++ Racer/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace Racer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.carorange = new System.Windows.Forms.PictureBox();
+            this.carblue = new System.Windows.Forms.PictureBox();
+            this.mycar = new System.Windows.Forms.PictureBox();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.carorange)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.carblue)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mycar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(84, -15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(17, 108);
+            this.label1.TabIndex = 0;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.SystemColors.Control;
+            this.label2.Location = new System.Drawing.Point(84, 113);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(17, 108);
+            this.label2.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.SystemColors.Control;
+            this.label3.Location = new System.Drawing.Point(84, 241);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(17, 108);
+            this.label3.TabIndex = 2;
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.SystemColors.Control;
+            this.label4.Location = new System.Drawing.Point(84, 374);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(17, 108);
+            this.label4.TabIndex = 3;
+            // 
+            // carorange
+            // 
+            this.carorange.Image = global::Racer.Properties.Resources.car21;
+            this.carorange.Location = new System.Drawing.Point(135, 12);
+            this.carorange.Name = "carorange";
+            this.carorange.Size = new System.Drawing.Size(43, 54);
+            this.carorange.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.carorange.TabIndex = 6;
+            this.carorange.TabStop = false;
+            // 
+            // carblue
+            // 
+            this.carblue.Image = global::Racer.Properties.Resources.car11;
+            this.carblue.Location = new System.Drawing.Point(24, 39);
+            this.carblue.Name = "carblue";
+            this.carblue.Size = new System.Drawing.Size(43, 54);
+            this.carblue.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.carblue.TabIndex = 5;
+            this.carblue.TabStop = false;
+            // 
+            // mycar
+            // 
+            this.mycar.Image = global::Racer.Properties.Resources.car3;
+            this.mycar.Location = new System.Drawing.Point(107, 325);
+            this.mycar.Name = "mycar";
+            this.mycar.Size = new System.Drawing.Size(47, 57);
+            this.mycar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.mycar.TabIndex = 4;
+            this.mycar.TabStop = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Impact", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.ForeColor = System.Drawing.Color.Red;
+            this.label5.Location = new System.Drawing.Point(35, 150);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(134, 34);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "GAME OVER";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(190, 394);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.carorange);
+            this.Controls.Add(this.carblue);
+            this.Controls.Add(this.mycar);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Гоночнки";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.carorange)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.carblue)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mycar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.PictureBox mycar;
+        private System.Windows.Forms.PictureBox carblue;
+        private System.Windows.Forms.PictureBox carorange;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Racer/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Racer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Label[] road = new Label[4];
+        Random random = new Random();
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            for (int i = 0; i < road.Length; i++)
+            {
+                road[i].Top += 2;
+                if (road[i].Top > Height)
+                    road[i].Top = -road[i].Height;
+            }
+
+            carblue.Top += 3;
+            if (carblue.Top > Height)
+            {
+                carblue.Top = -carblue.Height;
+                carblue.Left = random.Next(0, Width - carblue.Width);
+            }
+
+
+            carorange.Top += 4;
+            if (carorange.Top > Height)
+                carorange.Top = -carorange.Height;
+
+            if (mycar.Bounds.IntersectsWith(carblue.Bounds))
+            {
+                GameOver();
+
+            }
+            if (mycar.Bounds.IntersectsWith(carorange.Bounds))
+            {
+                GameOver();
+
+            }
+        }
+
+        private void GameOver()
+        {
+            label5.Visible = true;
+            timer1.Stop();
+            DialogResult dialogResult = MessageBox.Show("Еще?", "ты проиграл", MessageBoxButtons.YesNo);
+            if (dialogResult == DialogResult.Yes)
+                Restart();
+            else
+                Close();
+        }
+
+        void Restart()
+        {
+            carblue.Top = -carblue.Height;
+            carorange.Top = -carorange.Height;
+            timer1.Start();
+            label5.Visible = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label5.Visible = false;
+            road[0] = label1;
+            road[1] = label2;
+            road[2] = label3;
+            road[3] = label4;
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left && mycar.Left > 0)
+                mycar.Left -= 5;
+            if (e.KeyCode == Keys.Right && mycar.Left < Width-mycar.Width)
+                mycar.Left += 5;
+        }
+    }
+}
